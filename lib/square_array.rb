5bbdb0c82cd896892cb6fe93982fe27309778838
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-15 04:53:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count=0
   new_array=[]
   while count<array.length do
-    new_array.push(count)=2**array[count]
+    new_array.push(2**array[count])
     count +=1
   end
   new_array
